e6ad7852f334b945f562394ee23830f8ddc8d48f
FlorianCliquet <florian.cliquet.pro@gmail.com>
2024-03-18 10:15:54 +0100

    start

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>TheGoodReviews</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php
+?>
+</body>
+</html>
